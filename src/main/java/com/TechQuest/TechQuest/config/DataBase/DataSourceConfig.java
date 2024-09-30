d10b653e162123ded42259c5e3558dc3a1e86f9e
BUKODONOZOR <jafetdavidibarguenlozano@pascualbravo.edu.co>
2024-09-30 13:29:14 -0500

    feat creation of Postgres entity Manager Factory and Postgres DataSource

--- src/main/java/com/TechQuest/TechQuest/config/DataBase/DataSourceConfig.java
+++ src/main/java/com/TechQuest/TechQuest/config/DataBase/DataSourceConfig.java
@@ -4,20 +4,36 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
 
+    @Primary
     @Bean("mysqlProperties")
     @ConfigurationProperties(prefix = "spring.datasource.mysql")
-    public DataSourceProperties getMySqlProperties() {
+    public DataSourceProperties getMysqlProperties() {
         return new DataSourceProperties();
     }
 
+    @Primary
     @Bean("mysqlDatasource")
     public DataSource getMysqlDatasource() {
-        return getMySqlProperties().initializeDataSourceBuilder().build();
+        DataSourceProperties mysqlProps = getMysqlProperties();
+        return mysqlProps.initializeDataSourceBuilder().build();
+    }
+
+    @Bean("postgresqlProperties")
+    @ConfigurationProperties(prefix = "spring.datasource.postgresql")
+    public DataSourceProperties getPostgresqlProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean("postgresqlDatasource")
+    public DataSource getPostgresqlDatasource() {
+        DataSourceProperties postgresqlProps = getPostgresqlProperties();
+        return postgresqlProps.initializeDataSourceBuilder().build();
     }
 }
